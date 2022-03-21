16ac519157b8ab481f232c7ac3b30ebd8cd15f2b
Victor Bondarenko <nirasimha@gmail.com>
2022-03-21 10:00:41 -0400

    add loop to show all todos list

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-let command = prompt('Please enter a command')
-while (command !== 'quit') {
-  command = prompt('Please enter a command')
+let command = prompt('Please enter a command');
+const todos = ['clean the house', 'clean the backyard', 'wash a car'];
+
+
+while (command !== 'quit' && command !== 'q') {
+  if(command === 'list'){
+    console.log('********************************');
+    todos.forEach((task, i) => {
+      console.log(`${i}: ${task}`);
+    });
+    console.log('********************************');
+  }
+  command = prompt('Please enter a command');
+
   
 }
 console.log('OK, You quit!');
